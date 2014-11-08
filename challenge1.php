07bc1387f8275b19171f0627cfc836ffb55419f5
sunshinekitty <edwards@linux.com>
2014-11-08 10:40:26 -0600

    Add first challenge file

---
+++ challenge1.php
@@ -0,0 +1,94 @@
+<?php
+/*
+ * Alex Edwards (sunshinekitty)
+ * Nov, 2014
+ * challenge1.php
+ *
+ * This script creates a 512MB Cloud Server and then returns the root password and IP address.
+*/
+
+// Require composer's copy of php-opencloud
+require '../vendor/autoload.php';
+
+use OpenCloud\Rackspace;
+use OpenCloud\Compute\Constants\ServerState;
+use Guzzle\Http\Exception\BadResponseException;
+
+// Load in creds
+$creds = parse_ini_file('./.rackspace_credentials');
+
+// Create Rackspace client object
+$client = new Rackspace(Rackspace::US_IDENTITY_ENDPOINT, array(
+  'username' => $creds['username'],
+  'apiKey'   => $creds['apikey']
+));
+
+// Create compute service from Rackspace client in ORD
+$service = $client->computeService(null, 'ORD');
+
+// Service for actually making the server
+$server = $service->server();
+
+// Select OS
+$images = $service->imageList();
+foreach ($images as $image) 
+{
+  if (strpos($image->name, 'Ubuntu') !== false) 
+  {
+    $serverImage = $image;
+    break;
+  }
+}
+
+// Get 512M
+$flavors = $service->flavorList();
+foreach ($flavors as $flavor) 
+{
+    if (strpos($flavor->name, '512M') !== false) 
+    {
+    $serverFlavor = $flavor;
+    break;
+    }
+}
+
+// Obtain server name
+while (true) 
+{ // Infinite loop until we get proper input
+  echo "Enter a name for the server: ";
+  $serverName = trim(fgets(STDIN));
+  if ( strlen($serverName) > 1 ) 
+  {
+    break;
+  }
+}
+echo "Thanks! Creation process started.\n";
+
+// Create server
+try 
+{
+  $response = $server->create(array(
+    'name'    =>  $serverName,
+    'image'   =>  $serverImage,
+    'flavor'  =>  $serverFlavor
+  ));
+} 
+catch (BadRequestException $f) 
+{ // If creation fails
+  $response = $f->getResponse();
+  echo sprintf(
+    'Status: %s\nBody: %s\nHeaders: %s',
+    $response->getStatusCode(),
+    $response->getBody(true),
+    implode(', ', $response->getHeaderLines())
+  );
+}
+
+// Wait for server creation to finish
+$server->waitFor(ServerState::ACTIVE);
+
+// Print out IP and Root Password
+echo "Root password: " . $server->adminPass;
+echo "\n";
+echo "IP Address: " . $server->accessIPv4;
+
+?>
